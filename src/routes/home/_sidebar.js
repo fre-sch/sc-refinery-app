b94043af2e554e7b9a25fdd42ff41c7b29a8cd5b
Yurima1 <103342881+Yurima1@users.noreply.github.com>
2022-04-15 12:24:50 +0200

    Sidebar

---
+++ src/routes/home/_sidebar.js
@@ -0,0 +1,44 @@
+import { Link } from "preact-router/match"
+import { Icon } from "../../components/icon"
+import { Sidebar } from "../../components/sidebar"
+import constants from "../../constants"
+
+const SidebarItem = ({ label, href, icon }) => (
+  <li class="nav-item">
+    <Link href={href} class="nav-link" activeClassName="active">
+      <Icon cls={icon} />
+      {label}
+    </Link>
+  </li>
+)
+
+const userSidebar = () => {
+  return (
+    <Sidebar>
+      <ul class="nav nav-pills flex-column scrollarea">
+        <SidebarItem
+          label="Friends"
+          href={constants.BASEURL + "/home"}
+          icon="person-fill"
+        />
+        <SidebarItem
+          label="Mining Session"
+          href={constants.BASEURL + "/home/Plazhalter"}
+          icon="bucket-fill"
+        />
+        <SidebarItem
+          label="Outstanding Payments"
+          href={constants.BASEURL + "/home/mining_session/"}
+          icon="bucket-fill"
+        />
+        <SidebarItem
+          label="Settings"
+          href={constants.BASEURL + "/home/settings"}
+          icon="bucket-fill"
+        />
+      </ul>
+    </Sidebar>
+  )
+}
+
+export default userSidebar
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,11 +1,24 @@
-import { h } from 'preact';
-import style from './style.css';
+import { Router } from 'preact-router'
+import constants from "../../constants"
+import Sidebar from "./_sidebar"
+import MiningSession from './mining_session/index'
 
-const Home = () => (
-	<div class={style.home}>
-		<h1>Home</h1>
-		<p>Das ist eine Nachricht für Frechu, "Du Geiles Stück DNA" .</p>
-	</div>
-);
 
-export default Home;
+const Default = () => (
+  <div class="p-3">Welcome</div>
+)
+
+const homeIndex = () => {
+  return (
+    <div class="d-flex flex-grow-1">
+      <Sidebar />
+      <Router>
+        <Default path={constants.BASEURL + "/home"} />
+
+        <MiningSession path={constants.BASEURL + "/home/mining_session"} />
+      </Router>
+    </div>
+  )
+}
+
+export default homeIndex
